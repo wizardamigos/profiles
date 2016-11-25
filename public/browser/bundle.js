c6abeda7cb80e808e8b68095a6250cdb50733da9
Alexander Praetorius <dev@serapath.de>
2016-11-25 10:34:29 +0100

    ADD relative url to config

--- public/browser/bundle.js
+++ public/browser/bundle.js
@@ -1,1 +1,36 @@
-alert('hello peer! :-)')
+var config = location.href.split('/').pop().join('/') + '/config.json'
+
+ajax(config, function (data) {
+  document.body.innerHTML = `
+    <h1>welcome peer! :-)</h1>
+    <xmp>
+      ${JSON.stringify(data, null, 2)}
+    </xmp>
+  `
+})
+
+/******************************************************************************
+  HELPER
+******************************************************************************/
+function ajax (params, callback) {
+  var url = typeof params === 'string' ? params : params.url
+  var method = params.method || (params.data ? 'POST': 'GET')
+  var body = params.data
+  var H = params.headers ? params.headers : params.body ? {
+    'X-Requested-With' :'XMLHttpRequest',
+    'Content-Type'     :'application/x-www-form-urlencoded'
+  } : {}
+  var xhr = new XMLHttpRequest()
+  if (!xhr) throw new Error('No AJAX support')
+  xhr.open(method, url)
+  for (var key in H) xhr.setRequestHeader(key, H[key])
+  xhr.onload = xhr.onerror = function (response) {
+    var Hjson = {}, h = xhr.getAllResponseHeaders()
+    ;(h.match(/([^\n\r:]+):([^\n\r]+)/g)||[]).forEach(function(item){
+      var tmp = item.split(': ')
+      Hjson[tmp[0]] = tmp[1]
+    })
+    if (callback) callback(this.response, response, xhr, Hjson)
+  }
+  xhr.send(body||null)
+}
